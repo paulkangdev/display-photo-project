68c340462f9ff465c24371841ef81af37666146a
Paul Kang <pkang29@gmail.com>
2023-09-19 09:02:23 -0500

    remove sleep function

--- src/api/useApi.ts
+++ src/api/useApi.ts
@@ -5,8 +5,5 @@ const axiosClient = axios.create();
 axiosClient.defaults.baseURL = "https://jsonplaceholder.typicode.com/";
 
 export async function fetchAllPhotos() {
-    const sleep = (ms:number) => new Promise(
-    resolve => setTimeout(resolve, ms));
-    await sleep(5000);
     return axiosClient.get("photos").then((res) => res);
 }
